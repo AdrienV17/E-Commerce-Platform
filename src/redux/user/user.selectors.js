4f321b7ff4919711f6873cb1d8ba293fb5b74214
AdrienV17 <hernanadrianv17@gmail.com>
2020-06-04 22:54:19 -0500

    Resetting

--- src/redux/user/user.selectors.js
+++
@@ -1,9 +0,0 @@
-import { createSelector } from "reselect";
-
-const selectUser = state => state.user;
-
-const selectCart = state => state.cart;
-
-export const selectCurrentUser = createSelector([selectUser],(user)=> user.selectCurrentUser)
-
-export const selectCartHidden = createSelector([selectCart], cart=> cart.hidden)
--- src/test.js
+++
@@ -1,6 +0,0 @@
-import firebase from 'firebase/app';
-import 'firebase/firestore';
-
-const firestore = firebase.firestore();
-
-firestore.collection('users').doc('FTNxppEZ0nU5T4yt7S4w')

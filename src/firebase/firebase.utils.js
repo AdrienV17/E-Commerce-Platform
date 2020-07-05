2ec594657269779b285bdd752a6a14ab5a9fb224
AdrienV17 <hernanadrianv17@gmail.com>
2020-07-04 23:23:13 -0500

    updated app component, sign-in component and sign-up component with Hooks

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -12,7 +12,8 @@ const config = {
   appId: "1:200687351023:web:5a8ee1929c18cbfb10171a",
 };
 firebase.initializeApp(config);
-
+export const firestore = firebase.firestore();
+export const auth = firebase.auth();
 export const createUserProfileDocument = async (userAuth, additionalData) => {
   if (!userAuth) return;
 
@@ -80,8 +81,8 @@ export const getCurrentUser = () => {
   });
 };
 
-export const auth = firebase.auth();
-export const firestore = firebase.firestore();
+ 
+
 
 export const googleProvider = new firebase.auth.GoogleAuthProvider();
 googleProvider.setCustomParameters({ prompt: 'select_account' });

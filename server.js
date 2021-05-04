8a04fbab3116b4756846224627cf72db642e165a
Hernán Valencia <adrienv17dom@gmail.com>
2021-05-03 23:52:27 -0500

    BackEnd ready

--- server.js
+++ server.js
@@ -1,38 +1,41 @@
-const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-const path = require('path');
-const enforce = require('express-sslify');
-
-if (process.env.NODE_ENV !== 'production') require('dotenv').config();
-
-const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
-
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const enforce = require("express-sslify");
 const app = express();
+
+if (process.env.NODE_ENV !== "production") require("dotenv").config();
+
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+
 const port = process.env.PORT || 5000;
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+if(process.env.NODE_ENV === 'production'){
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(enforce.HTTPS({ trustProtoHeader: true }));
+  app.use(enforce.HTTPS({ trustProtoHeader: true }));
+}
 app.use(cors());
-// if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'client/build')));
+app.use(express.static(path.join(__dirname, "build")));
 
-  app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-  });
-// }
-
-
-app.get('/service-worker.js', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '..', 'build', 'service-worker.js'));
+app.get("*", function (req, res, next) {
+  const route = req.url.split("/")[1];
+  if (route !== "payment" || route !== "service-worker.js") {
+    res.sendFile(path.join(__dirname, "build", "index.html"));
+  } else {
+    next();
+  }
 });
 
-app.post('/payment', (req, res) => {
+app.get("/service-worker.js", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "..", "build", "service-worker.js"));
+});
+
+app.post("/payment", (req, res) => {
   const body = {
     source: req.body.token.id,
     amount: req.body.amount,
-    currency: 'usd'
+    currency: "usd",
   };
 
   stripe.charges.create(body, (stripeErr, stripeRes) => {
@@ -44,7 +47,7 @@ app.post('/payment', (req, res) => {
   });
 });
 
-app.listen(port, error => {
+app.listen(port, (error) => {
   if (error) throw error;
-  console.log('Server running on port ' + port);
+  console.log("Server running on port " + port);
 });
